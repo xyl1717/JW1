a1efdb54f19bf82310883b955a101ca983529d37
xyl1717 <jwmckvn@gmail.com>
2024-09-09 13:50:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Unit2.TemperatureConverter.Starter
+
+In this project, you will be building out the functionality of a simple app that asks a user for some numbers and then calculates some temperature conversions according to the user's input.
+
+## Instructions
+
+1. Create a new repository by using this starter repo as a template.
+2. Clone down your version.
+3. Open the project in VS Code.
+4. Live serve `index.html`. You should be greeted with a prompt.
+5. Complete the functions described in `index.js`!
+
+## Submission
+
+Submit a link to your Github repo.
---
+++ index.js
@@ -0,0 +1,64 @@
+// Complete the following functions to make our program work!
+
+/**
+ * Converts the given Fahrenheit temperature `f` to Celsius.
+ * @param {number} f temperature in °F
+ * @returns {number} temperature in °C
+ */
+function convertToCelsius(f) {
+  // TODO
+}
+
+/**
+ * | Temperature | Description |
+ * | ----------- | ----------- |
+ * | < 32        | "very cold" |
+ * | < 64        | "cold"      |
+ * | < 86        | "warm"      |
+ * | < 100       | "hot"       |
+ * | >= 100      | "very hot"  |
+ *
+ * @param {number} f temperature in °F
+ * @returns {string} the description from the table above corresponding to
+ * the given Fahrenheit temperature `f`
+ */
+function describeTemperature(f) {
+  // TODO
+}
+
+/**
+ * @param {number} limit
+ * @returns {number} a random integer in the range [0, `limit`)
+ */
+function getRandomInt(limit) {
+  // TODO
+}
+
+// -------------------- DO NOT CHANGE THE CODE BELOW ---------------------- //
+/**
+ * Converts the given temperature from Fahrenheit to Celsius,
+ * then alerts the user with a descriptive message.
+ * @param {number} f temperature in °F
+ */
+function parseFahrenheit(f) {
+  const c = convertToCelsius(f);
+  const description = describeTemperature(f);
+  const message = `${f}°F is ${c}°C. That is ${description}.`;
+  alert(message);
+}
+
+const fahrenheitPrompt =
+  "Please enter a number. We will convert that temperature from Fahrenheit to Celsius.";
+let f = prompt(fahrenheitPrompt);
+parseFahrenheit(+f);
+
+alert("Let's try that again.");
+f = prompt(fahrenheitPrompt);
+parseFahrenheit(+f);
+
+alert("Let's try some random temperatures.");
+f = getRandomInt(110);
+parseFahrenheit(f);
+
+f = getRandomInt(110);
+parseFahrenheit(f);
